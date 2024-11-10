bda84dc6aeed81ca371da0f1b3a9458cac34f360
ronnygomezp21 <ronny.gomezpe@ug.edu.ec>
2024-11-10 08:30:11 -0500

    feat:cambios

--- src/app/home/home/home.component.ts
+++ src/app/home/home/home.component.ts
@@ -1,18 +1,18 @@
-import { Component } from '@angular/core';
-import { RouterModule } from '@angular/router';
-import { NavbarComponent } from "../../shared/navbar/navbar.component";
-import { SidebarComponent } from "../../shared/sidebar/sidebar.component";
-import { QuestionsComponent } from "../../questions/questions/questions.component";
-import { HttpClientModule } from '@angular/common/http';
-import { HomeQuestionComponent } from '../../shared/home-question/home-question.component';
+// import { Component } from '@angular/core';
+// import { RouterModule } from '@angular/router';
+// import { NavbarComponent } from "../../shared/navbar/navbar.component";
+// import { SidebarComponent } from "../../shared/sidebar/sidebar.component";
+// import { QuestionsComponent } from "../../questions/questions/questions.component";
+// import { HttpClientModule } from '@angular/common/http';
+// import { HomeQuestionComponent } from '../../shared/home-question/home-question.component';
 
-@Component({
-  selector: 'app-home',
-  standalone: true,
-  imports: [RouterModule, NavbarComponent, SidebarComponent, QuestionsComponent, HttpClientModule, HomeQuestionComponent],
-  templateUrl: './home.component.html',
-  styleUrl: './home.component.css'
-})
-export class HomeComponent {
+// @Component({
+//   selector: 'app-home',
+//   standalone: true,
+//   imports: [RouterModule, NavbarComponent, SidebarComponent, QuestionsComponent, HttpClientModule, HomeQuestionComponent],
+//   templateUrl: './home.component.html',
+//   styleUrl: './home.component.css'
+// })
+// export class HomeComponent {
 
-}
+// }
