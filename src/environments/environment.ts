5516145914e9aeb563a1b7c9e9caa679ff2017d9
ronnygomezp21 <ronny.gomezpe@ug.edu.ec>
2025-01-21 21:53:47 -0500

    fix: cambio de endpoint y descarga de excel.

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,5 +1,5 @@
 export const environment = {
     production: false,
-    //apiUrl: 'http://localhost:80/fruit/api/',
-    apiUrl: 'http://devhubcentral.org/fruit/api/',
+    apiUrl: 'http://localhost:80/fruit/api/',
+    //apiUrl: 'http://devhubcentral.org/fruit/api/',
 };
