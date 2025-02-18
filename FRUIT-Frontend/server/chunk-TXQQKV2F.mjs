10ab474dbc03a5356e640008fd59d1747d09d44e
ronaldoja <ronaldojavier0701@gmail.com>
2025-02-17 21:58:00 -0500

    Auto-generated commit

--- FRUIT-Frontend/server/chunk-TXQQKV2F.mjs
+++
@@ -1,2 +0,0 @@
-import './polyfills.server.mjs';
-import{U as o}from"./chunk-RDXAN74U.mjs";var n=class t{constructor(){}setItem(e){typeof window<"u"&&window.localStorage&&localStorage.setItem("language",e)}getItem(){return typeof window<"u"&&window.localStorage?localStorage.getItem("language")||"es":null}static \u0275fac=function(r){return new(r||t)};static \u0275prov=o({token:t,factory:t.\u0275fac,providedIn:"root"})};export{n as a};
--- FRUIT-Frontend/server/main.server.mjs
+++
@@ -1,2 +0,0 @@
-import './polyfills.server.mjs';
-import{a}from"./chunk-TDRBKEEP.mjs";import"./chunk-KCDCPOIC.mjs";import"./chunk-7ZI5CKKE.mjs";import"./chunk-HFC2F4Q4.mjs";import"./chunk-J4BDXTMW.mjs";import"./chunk-QZQANR5E.mjs";import"./chunk-XZVO5RUM.mjs";import"./chunk-G2EMQGJ6.mjs";import"./chunk-UTIOHHII.mjs";import"./chunk-RDXAN74U.mjs";import"./chunk-5XUXGTUW.mjs";export{a as default};
